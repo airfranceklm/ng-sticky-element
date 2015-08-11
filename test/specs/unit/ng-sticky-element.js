a60950efe5f75d56657f2aff4fc430bf544f277c
Tamer Aydin <tamer.aydin@gmail.com>
2015-08-11 09:55:27 +0200

    improved test specs

--- test/specs/unit/ng-sticky-element.js
+++ test/specs/unit/ng-sticky-element.js
@@ -2,26 +2,41 @@
 
 describe('ng-sticky-element', function() {
 
-  beforeEach(module('afklStickyElement'));
-
   var element,
     rootScope,
-    compile;
+    compile,
+    timeout,
+    global = jasmine.getGlobal();
+
+  global.requestAnimationFrame = jasmine.createSpy('requestAnimationFrame');
+  global.cancelAnimationFrame = jasmine.createSpy('cancelAnimationFrame');
+
+  beforeEach(function() {
+    module('afklStickyElement');
+    inject(function($injector, _$rootScope_, _$compile_) {
+      timeout = $injector.get('$timeout');
+      rootScope = _$rootScope_;
+      compile = _$compile_;
+
+      element = compile('<div afkl-sticky-element></div>')(rootScope);
+      rootScope.$digest();
+    });
+  });
 
   describe('afklStickyElement.directives', function() {
 
-    beforeEach(function() {
-      inject(function(_$rootScope_, _$compile_) {
-        rootScope = _$rootScope_;
-        compile = _$compile_;
-
-        element = compile('<div afkl-sticky-element></div>')(rootScope);
-        rootScope.$digest();
-      });
+    it('should be initialized properly', function() {
+      expect(element.hasClass('afkl-sticky-element')).toBeTruthy();
     });
 
-    it('should be initialised properly', function() {
-      expect(element.hasClass('afkl-sticky-element')).toBeTruthy();
+    it('should start requesting animation frame', function() {
+      timeout.flush();
+      expect(global.requestAnimationFrame).toHaveBeenCalled();
+    });
+
+    it('should stop requesting animation frame on $destroy', function() {
+      element.remove();
+      expect(global.cancelAnimationFrame).toHaveBeenCalled();
     });
   });
 });
